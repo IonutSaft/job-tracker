6ec1a552c3fa011f3f9191f203065d402195367b
IonutSaft <ionut.saft14@gmail.com>
2026-06-20 20:19:37 +0300

    Installed vitest and configured hostname for images

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import { logout } from "@/lib/actions/auth";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
@@ -12,16 +13,27 @@ export default async function Home() {
   } = await supabase.auth.getUser();
 
   const displayName = user?.user_metadata.full_name ?? "Unknown User";
+  const userAvatar = user?.user_metadata.avatar_url ?? "";
 
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       {user ? (
         <div className="flex flex-col items-center gap-4">
-          <p className="test-lg">
+          {userAvatar && (
+            <Image
+              src={userAvatar}
+              alt="User Avatar"
+              className="h-16 w-16 rounded-full"
+              width={64}
+              height={64}
+              priority
+            ></Image>
+          )}
+          <p className="text-lg">
             Hello, <span className="font-semibold">{displayName}</span>!
           </p>
           <form action={logout}>
-            <button className="test-sm text-muted-foreground underline underline-offset-4 hover:text-foreground">
+            <button className="text-sm text-muted-foreground underline underline-offset-4 hover:text-foreground">
               Logout
             </button>
           </form>
